5d16f2a875d37fdcb1fd3ead51eec06eabd0bfca
AlexanderCeli <51131915+AlexanderCeli@users.noreply.github.com>
2020-08-21 14:48:46 -0500

    Tarea Bimestral

---
+++ src/java/modelo/Clientes.java
@@ -0,0 +1,150 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Alexander
+ */
+@Entity
+@Table(name = "clientes")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Clientes.findAll", query = "SELECT c FROM Clientes c")
+    , @NamedQuery(name = "Clientes.findByCedula", query = "SELECT c FROM Clientes c WHERE c.cedula = :cedula")
+    , @NamedQuery(name = "Clientes.findByNombre", query = "SELECT c FROM Clientes c WHERE c.nombre = :nombre")
+    , @NamedQuery(name = "Clientes.findByApellido", query = "SELECT c FROM Clientes c WHERE c.apellido = :apellido")
+    , @NamedQuery(name = "Clientes.findByDireccion", query = "SELECT c FROM Clientes c WHERE c.direccion = :direccion")
+    , @NamedQuery(name = "Clientes.findByEdad", query = "SELECT c FROM Clientes c WHERE c.edad = :edad")
+    , @NamedQuery(name = "Clientes.findByVehiculo", query = "SELECT c FROM Clientes c WHERE c.vehiculo = :vehiculo")
+    , @NamedQuery(name = "Clientes.findByProvincia", query = "SELECT c FROM Clientes c WHERE c.provincia = :provincia")})
+public class Clientes implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "cedula")
+    private Integer cedula;
+    @Size(max = 45)
+    @Column(name = "nombre")
+    private String nombre;
+    @Size(max = 45)
+    @Column(name = "apellido")
+    private String apellido;
+    @Size(max = 45)
+    @Column(name = "direccion")
+    private String direccion;
+    @Column(name = "edad")
+    private Integer edad;
+    @Size(max = 45)
+    @Column(name = "vehiculo")
+    private String vehiculo;
+    @Size(max = 45)
+    @Column(name = "provincia")
+    private String provincia;
+
+    public Clientes() {
+    }
+
+    public Clientes(Integer cedula) {
+        this.cedula = cedula;
+    }
+
+    public Integer getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(Integer cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public String getVehiculo() {
+        return vehiculo;
+    }
+
+    public void setVehiculo(String vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (cedula != null ? cedula.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Clientes)) {
+            return false;
+        }
+        Clientes other = (Clientes) object;
+        if ((this.cedula == null && other.cedula != null) || (this.cedula != null && !this.cedula.equals(other.cedula))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "modelo.Clientes[ cedula=" + cedula + " ]";
+    }
+    
+}
---
+++ src/java/modelo/Vehiculo.java
@@ -0,0 +1,163 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Alexander
+ */
+@Entity
+@Table(name = "vehiculo")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Vehiculo.findAll", query = "SELECT v FROM Vehiculo v")
+    , @NamedQuery(name = "Vehiculo.findById", query = "SELECT v FROM Vehiculo v WHERE v.id = :id")
+    , @NamedQuery(name = "Vehiculo.findByPlaca", query = "SELECT v FROM Vehiculo v WHERE v.placa = :placa")
+    , @NamedQuery(name = "Vehiculo.findByA\u00f1o", query = "SELECT v FROM Vehiculo v WHERE v.a\u00f1o = :a\u00f1o")
+    , @NamedQuery(name = "Vehiculo.findByNPasajeros", query = "SELECT v FROM Vehiculo v WHERE v.nPasajeros = :nPasajeros")
+    , @NamedQuery(name = "Vehiculo.findByConcesonario", query = "SELECT v FROM Vehiculo v WHERE v.concesonario = :concesonario")
+    , @NamedQuery(name = "Vehiculo.findByMarca", query = "SELECT v FROM Vehiculo v WHERE v.marca = :marca")
+    , @NamedQuery(name = "Vehiculo.findByModelo", query = "SELECT v FROM Vehiculo v WHERE v.modelo = :modelo")
+    , @NamedQuery(name = "Vehiculo.findByPaisFabrica", query = "SELECT v FROM Vehiculo v WHERE v.paisFabrica = :paisFabrica")})
+public class Vehiculo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "id")
+    private Integer id;
+    @Size(max = 45)
+    @Column(name = "placa")
+    private String placa;
+    @Size(max = 45)
+    @Column(name = "a\u00f1o")
+    private String año;
+    @Size(max = 45)
+    @Column(name = "n_pasajeros")
+    private String nPasajeros;
+    @Size(max = 45)
+    @Column(name = "concesonario")
+    private String concesonario;
+    @Size(max = 45)
+    @Column(name = "marca")
+    private String marca;
+    @Size(max = 45)
+    @Column(name = "modelo")
+    private String modelo;
+    @Size(max = 45)
+    @Column(name = "paisFabrica")
+    private String paisFabrica;
+
+    public Vehiculo() {
+    }
+
+    public Vehiculo(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getAño() {
+        return año;
+    }
+
+    public void setAño(String año) {
+        this.año = año;
+    }
+
+    public String getNPasajeros() {
+        return nPasajeros;
+    }
+
+    public void setNPasajeros(String nPasajeros) {
+        this.nPasajeros = nPasajeros;
+    }
+
+    public String getConcesonario() {
+        return concesonario;
+    }
+
+    public void setConcesonario(String concesonario) {
+        this.concesonario = concesonario;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getPaisFabrica() {
+        return paisFabrica;
+    }
+
+    public void setPaisFabrica(String paisFabrica) {
+        this.paisFabrica = paisFabrica;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Vehiculo)) {
+            return false;
+        }
+        Vehiculo other = (Vehiculo) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "modelo.Vehiculo[ id=" + id + " ]";
+    }
+    
+}
